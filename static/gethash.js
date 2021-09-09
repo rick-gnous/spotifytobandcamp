7d259ceef8eb2d84bc768332e63f0a2c3f0e160c
rick <rick@gnous.eu>
2021-09-09 02:06:39 +0200

    refactor: add ;

--- static/gethash.js
+++ static/gethash.js
@@ -1,13 +1,14 @@
-const hash = window.location.hash.substring(1).split('&')
-var datas = {}
+const hash = window.location.hash.substring(1).split('&');
+var datas = {};
 for (let elem of hash) {
-  elem = elem.split('=')
-  datas[elem[0]] = elem[1]
+  elem = elem.split('=');
+  datas[elem[0]] = elem[1];
 }
+
 fetch('/mytoken', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
   },
   body: JSON.stringify(datas)
-}).then(() => {window.location.replace("/")})
+}).then(() => {window.location.replace("/")});
